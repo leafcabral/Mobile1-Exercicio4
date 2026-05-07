6aba38bacc5e0923b8b1c50270b76abe1d848bbe
leafcabral <rafaelcabral4100@gmail.com>
2026-05-07 12:40:45 -0300

    feat: add splash screen

---
+++ app/src/main/java/com/leafcabral/tasks/ui/SplashFragment.kt
@@ -0,0 +1,17 @@
+package com.leafcabral.tasks.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.leafcabral.tasks.R
+
+class SplashFragment : Fragment() {
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		return inflater.inflate(R.layout.fragment_splash, container, false)
+	}
+}
