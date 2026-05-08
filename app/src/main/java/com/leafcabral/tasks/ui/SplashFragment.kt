449b2648394dc3e261de6e7320534ae72aec4cb3
leafcabral <rafaelcabral4100@gmail.com>
2026-05-08 08:54:00 -0300

    feat: add login fragment and values

--- app/src/main/java/com/leafcabral/tasks/ui/SplashFragment.kt
+++ app/src/main/java/com/leafcabral/tasks/ui/SplashFragment.kt
@@ -6,12 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.leafcabral.tasks.R
+import com.leafcabral.tasks.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+	private var _binding: FragmentSplashBinding? = null
+	private val binding get() = _binding!!
+
 	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
+		inflater: LayoutInflater,
+		container: ViewGroup?,
 		savedInstanceState: Bundle?
-	): View? {
-		return inflater.inflate(R.layout.fragment_splash, container, false)
+	): View {
+		_binding = FragmentSplashBinding.inflate(inflater, container, false)
+		return binding.root
+		/*return inflater.inflate(R.layout.fragment_splash, container, false)*/
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
 	}
 }
