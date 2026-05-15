17b04ad0189135bf6f9022349da22c218e45a3b7
leafcabral <rafaelcabral4100@gmail.com>
2026-05-15 08:58:29 -0300

    feat: add connections between some fragments

--- app/src/main/java/com/leafcabral/tasks/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/leafcabral/tasks/ui/auth/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.leafcabral.tasks.R
 import com.leafcabral.tasks.databinding.FragmentLoginBinding
 import com.leafcabral.tasks.databinding.FragmentRegisterBinding
@@ -20,5 +21,22 @@ class LoginFragment : Fragment() {
 	): View {
 		_binding = FragmentLoginBinding.inflate(inflater, container, true)
 		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		binding.buttonRegister.setOnClickListener {
+			findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+		}
+		binding.buttonRecover.setOnClickListener {
+			findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+		}
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+
+		_binding = null
 	}
 }
